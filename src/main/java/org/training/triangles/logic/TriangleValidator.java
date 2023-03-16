9ec4cc1c421c2f62652950e2ea85d72a906fabd1
zap24 <zap24@inbox.ru>
2023-03-16 22:07:07 +0300

    add new logic in TriangleValidator.java, in method isAcuteTriangle()

--- src/main/java/org/training/triangles/logic/TriangleValidator.java
+++ src/main/java/org/training/triangles/logic/TriangleValidator.java
@@ -23,12 +23,32 @@ public class TriangleValidator {
         }
 
     // проверка является ли треугольник равносторонним
-    public boolean isEquilateralTriangle () {
-        return true;
+    public boolean isEquilateralTriangle (Triangle triangle) {
+        Calculator calculator = new Calculator();
+        double firstSide = calculator.calculateDistanceBetweenPoints(triangle.getFirstPoint(), triangle.getSecondPoint());
+        double secondSide = calculator.calculateDistanceBetweenPoints(triangle.getSecondPoint(), triangle.getThirdPoint());
+        double thirdSide = calculator.calculateDistanceBetweenPoints(triangle.getFirstPoint(), triangle.getThirdPoint());
+        boolean isEquilateralTriangle = firstSide == secondSide & firstSide == thirdSide;
+        return isEquilateralTriangle;
     }
     // проверка является ли треугольник остроугольным
-    public boolean isAcuteTriangle() {
-        return true;
+    public boolean isAcuteTriangle(Triangle triangle) {
+        Calculator calculator = new Calculator();
+        double firstSide = calculator.calculateDistanceBetweenPoints(triangle.getFirstPoint(), triangle.getSecondPoint());
+        double secondSide = calculator.calculateDistanceBetweenPoints(triangle.getSecondPoint(), triangle.getThirdPoint());
+        double thirdSide = calculator.calculateDistanceBetweenPoints(triangle.getFirstPoint(), triangle.getThirdPoint());
+        double bigSide = Math.max(Math.max(firstSide, secondSide), thirdSide);
+        boolean isAcuteTriangle = false;
+        if (bigSide == firstSide) {
+            isAcuteTriangle = Math.pow(firstSide, 2) < (Math.pow(secondSide, 2) + Math.pow(thirdSide, 2));
+        }
+        if (bigSide == secondSide) {
+            isAcuteTriangle = Math.pow(secondSide, 2) < (Math.pow(firstSide, 2) + Math.pow(thirdSide, 2));
+        }
+        if (bigSide == thirdSide) {
+            isAcuteTriangle = Math.pow(thirdSide, 2) < (Math.pow(firstSide, 2) + Math.pow(secondSide, 2));
+        }
+        return isAcuteTriangle;
     }
     // проверка является ли треугольник тупоугольным
     public boolean isObtuseTriangle() {
